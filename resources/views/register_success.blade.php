6645a449ae0fa3151bf7f0928d5c672b2645d170
ailearning <admin@ailearning.id>
2026-02-24 09:08:33 +0700

    Update registration success page: simplify buttons

--- resources/views/register_success.blade.php
+++ resources/views/register_success.blade.php
@@ -308,14 +308,9 @@
         </div>
 
         <div class="buttons">
-            <a href="{{ route('register') }}" class="btn btn-primary">
-                <svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path>
-                </svg>
-                Daftar Siswa Lagi
+            <a href="{{ route('login') }}" class="btn btn-primary">
+                Masuk ke AI Learning
             </a>
-            <a href="{{ route('login') }}" class="btn btn-ghost">Login Admin</a>
         </div>
     </div>
 </body>
